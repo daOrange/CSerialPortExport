613d0fe7e56f0c5b9cef9c7fd4a7b4676000f447
程家山 <chengjs@tuya.com>
2021-01-08 20:48:13 +0800

    添加部分串口参数get、set方法

--- CSerialPortExport/export.cpp
+++ CSerialPortExport/export.cpp
@@ -6,9 +6,10 @@ cserialport_instance_t* cserialport_new() {
 	return instance;
 }
 
-void cserialport_init(cserialport_instance_t* instance, const char* portname, int baudRate) {
-	instance->sp->init(portname, baudRate);
+void cserialport_init(cserialport_instance_t* instance, const char* portname, int baudRate, Parity parity, DataBits dataBits, StopBits stopbits, int64 readBufferSize) {
+	instance->sp->init(portname, baudRate, parity, dataBits, stopbits, itas109::FlowNone, readBufferSize);
 	instance->sp->setMinByteReadNotify(1);
+	//instance->sp->setOperateMode(SynchronousOperate);
 }
 
 void cserialport_open(cserialport_instance_t* instance) {
@@ -20,45 +21,93 @@ void cserialport_close(cserialport_instance_t* instance) {
 }
 
 void cserialport_write(cserialport_instance_t* instance, const void* data, int length) {
-	instance->sp->writeData((const char*)data, length);
+	int a = instance->sp->writeData((const char*)data, length);
 }
 
 int cserialport_read(cserialport_instance_t* instance, char* data, int length) {
 	return instance->sp->readData((char*)data, length);
 }
 
-bool cserialport_isopen(cserialport_instance_t* instance) {
+bool cserialport_get_isopen(cserialport_instance_t* instance) {
 	return	instance->sp->isOpened();
 }
 
-void cserialport_setsync(cserialport_instance_t* instance) {
+void cserialport_set_portname(cserialport_instance_t* instance, const char* portname) {
+	instance->sp->setPortName(portname);
+}
+
+const char* cserialport_get_portname(cserialport_instance_t* instance) {
+	return instance->sp->getPortName().data();
+}
+
+void cserialport_set_baudrate(cserialport_instance_t* instance, int baudRate) {
+	instance->sp->setBaudRate(baudRate);
+}
+
+int cserialport_get_baudrate(cserialport_instance_t* instance) {
+	return instance->sp->getBaudRate();
+}
+
+void cserialport_set_parity(cserialport_instance_t* instance, itas109::Parity parity) {
+	instance->sp->setParity(parity);
+}
+
+itas109::Parity cserialport_get_parity(cserialport_instance_t* instance) {
+	return instance->sp->getParity();
+}
+
+void cserialport_set_databits(cserialport_instance_t* instance, itas109::DataBits dataBits) {
+	instance->sp->setDataBits(dataBits);
+}
+
+itas109::DataBits cserialport_get_databits(cserialport_instance_t* instance) {
+	return instance->sp->getDataBits();
+}
+
+void cserialport_set_stopbits(cserialport_instance_t* instance, itas109::StopBits stopbits) {
+	instance->sp->setStopBits(stopbits);
+}
+
+itas109::StopBits cserialport_get_stopbits(cserialport_instance_t* instance) {
+	return instance->sp->getStopBits();
+}
+
+void cserialport_set_readbuffersize(cserialport_instance_t* instance, int64 size) {
+	instance->sp->setReadBufferSize(size);
+}
+
+int64 cserialport_get_readbuffersize(cserialport_instance_t* instance) {
+	return instance->sp->getReadBufferSize();
+}
+
+void cserialport_set_sync(cserialport_instance_t* instance) {
 	instance->sp->setOperateMode(itas109::SynchronousOperate);
 }
 
-int cserialport_getbytestoread(cserialport_instance_t* instance) {
+int cserialport_get_bytestoread(cserialport_instance_t* instance) {
 	return instance->sp->getBytesToRead();
 }
 
-void cserialport_setdtr(cserialport_instance_t* instance, bool isenable) {
+void cserialport_set_dtr(cserialport_instance_t* instance, bool isenable) {
 	instance->sp->setDtr(isenable);
 }
 
-bool cserialport_getdtr(cserialport_instance_t* instance) {
+bool cserialport_get_dtr(cserialport_instance_t* instance) {
 	return instance->sp->getDtr();
 }
 
-void cserialport_setrts(cserialport_instance_t* instance, bool isenable) {
+void cserialport_set_rts(cserialport_instance_t* instance, bool isenable) {
 	instance->sp->setRts(isenable);
 }
 
-bool cserialport_getrts(cserialport_instance_t* instance) {
+bool cserialport_get_rts(cserialport_instance_t* instance) {
 	return instance->sp->getRts();
 }
 
-bool cserialport_getcts(cserialport_instance_t* instance) {
+bool cserialport_get_cts(cserialport_instance_t* instance) {
 	return instance->sp->getCts();
 }
 
-bool cserialport_getdsr(cserialport_instance_t* instance) {
+bool cserialport_get_dsr(cserialport_instance_t* instance) {
 	return instance->sp->getDsr();
 }
--- CSerialPortExport/export.h
+++ CSerialPortExport/export.h
@@ -15,7 +15,7 @@ typedef struct cserialport_instance_t {
 
 LIB_API cserialport_instance_t* cserialport_new();
 
-LIB_API void cserialport_init(cserialport_instance_t* instance, const char* portname, int baudRate);
+LIB_API void cserialport_init(cserialport_instance_t* instance, const char* portname, int baudRate, Parity parity, DataBits dataBits, StopBits stopbits, int64 readBufferSize);
 
 LIB_API void cserialport_open(cserialport_instance_t* instance);
 
@@ -25,21 +25,45 @@ LIB_API void cserialport_write(cserialport_instance_t* instance, const void* dat
 
 LIB_API int cserialport_read(cserialport_instance_t* instance, char* data, int length);
 
-LIB_API bool cserialport_isopen(cserialport_instance_t* instance);
+LIB_API bool cserialport_get_isopen(cserialport_instance_t* instance);
 
-LIB_API void cserialport_setsync(cserialport_instance_t* instance);
+LIB_API void cserialport_set_portname(cserialport_instance_t* instance, const char* portname);
 
-LIB_API int cserialport_getbytestoread(cserialport_instance_t* instance);
+LIB_API const char* cserialport_get_portname(cserialport_instance_t* instance);
 
-LIB_API void cserialport_setdtr(cserialport_instance_t* instance, bool isenable);
+LIB_API void cserialport_set_baudrate(cserialport_instance_t* instance, int baudRate);
 
-LIB_API bool cserialport_getdtr(cserialport_instance_t* instance);
+LIB_API int cserialport_get_baudrate(cserialport_instance_t* instance);
 
-LIB_API void cserialport_setrts(cserialport_instance_t* instance, bool isenable);
+LIB_API void cserialport_set_parity(cserialport_instance_t* instance, itas109::Parity parity);
 
-LIB_API bool cserialport_getrts(cserialport_instance_t* instance);
+LIB_API itas109::Parity cserialport_get_parity(cserialport_instance_t* instance);
 
-LIB_API bool cserialport_getcts(cserialport_instance_t* instance);
+LIB_API void cserialport_set_databits(cserialport_instance_t* instance, itas109::DataBits dataBits);
 
-LIB_API bool cserialport_getdsr(cserialport_instance_t* instance);
+LIB_API itas109::DataBits cserialport_get_databits(cserialport_instance_t* instance);
+
+LIB_API void cserialport_set_stopbits(cserialport_instance_t* instance, itas109::StopBits stopbits);
+
+LIB_API itas109::StopBits cserialport_get_stopbits(cserialport_instance_t* instance);
+
+LIB_API void cserialport_set_readbuffersize(cserialport_instance_t* instance, int64 size);
+
+LIB_API int64 cserialport_get_readbuffersize(cserialport_instance_t* instance);
+
+LIB_API void cserialport_set_sync(cserialport_instance_t* instance);
+
+LIB_API int cserialport_get_bytestoread(cserialport_instance_t* instance);
+
+LIB_API void cserialport_set_dtr(cserialport_instance_t* instance, bool isenable);
+
+LIB_API bool cserialport_get_dtr(cserialport_instance_t* instance);
+
+LIB_API void cserialport_set_rts(cserialport_instance_t* instance, bool isenable);
+
+LIB_API bool cserialport_get_rts(cserialport_instance_t* instance);
+
+LIB_API bool cserialport_get_cts(cserialport_instance_t* instance);
+
+LIB_API bool cserialport_get_dsr(cserialport_instance_t* instance);
 #endif
